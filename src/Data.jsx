be7d6f29985f87408d9e0a7df38410c05c8705ef
Vishnu P S <vishnu13three@gamil.com>
2025-02-21 22:38:17 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name:"Bessy Babu",
+        email:"bessybaby@gmail.com",
+        id: 1
+    },
+    {
+        name: "Muhammed Aslam",
+        email: "muhammedaslam@gmail.com",
+        id: 2
+    },
+    {
+        name: "Abin",
+        email:"abin@gmail.com",
+        id: 3
+    }
+]
