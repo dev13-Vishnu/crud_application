dea6776e4af5461e3ff94fa93193f624b80d62ee
Vishnu P S <vishnu13three@gamil.com>
2025-02-24 12:57:25 +0530

    cors configuration modified to allow frontend URL

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use('/uploads', express.static(path.join('uploads')));
 
 app.use('/api/users', userRoutes)
+app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
